00aec92e8cce86dc7e217880f7fcb2349886c7b4
BowenYao18 <by18@rice.edu>
2024-03-13 22:42:44 -0500

    tutorial

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # streamlit-tutorial
 
 This is a tutorial for the streamlit library.
+The tutorial can be found at [text](https://30days.streamlit.app/)
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,18 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
+from datetime import time, datetime
+import time as tm
+import os
+from io import StringIO
+
+# st.set_page_config(layout="wide")
 
 # Day 2
-st.header("Day2: Hello, World!")
+st.header("Day 2: Hello, World!")
 
 # Day 3
-st.header('Day3: st.button')
+st.header('Day 3: st.button')
 
 if st.button('Say hello'):
      st.write('Why hello there')
@@ -17,7 +23,7 @@ else:
 
 # Day 5
 
-st.header('Day5: st.write')
+st.header('Day 5: st.write')
 
 # Example 1
 
@@ -50,4 +56,233 @@ c = alt.Chart(df2).mark_circle().encode(
 st.write(c)
 
 
-# Day 6
+# Day 8
+st.header('Day 8: st.slider')
+
+# Example 1
+st.subheader('slider')
+age = st.slider('How old are you?', 0, 100, 20)
+st.write("I'm ", age, 'years old')
+
+# Example 2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# Example 3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(10, 00), time(10, 30)))
+st.write("You're scheduled for:", appointment)
+
+# Example 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="YYYY/MM/DD - hh:mm")
+st.write("Start time:", start_time)
+
+
+# Example 5
+st.subheader('Color slider')
+
+color = st.select_slider(
+    'Select a color of the rainbow',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+st.write('My favorite color is', color)
+
+start_color, end_color = st.select_slider(
+    'Select a range of color wavelength',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+    value=('red', 'blue'))
+st.write('You selected wavelengths between', start_color, 'and', end_color)
+
+
+st.header('Day 9: Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+st.header('Day 10: st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+st.header('Day 11: st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+st.header('Day 12: st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
+
+
+# import pandas_profiling
+# from streamlit_pandas_profiling import st_profile_report
+
+# st.header('Day 14: `streamlit_pandas_profiling`')
+
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+# pr = df.profile_report()
+# st_profile_report(pr)
+     
+
+st.header('Day 15: st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.title('Day 16: Customizing the theme of Streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#FE9C12"
+backgroundColor="#000000"
+secondaryBackgroundColor="#6E6EFE"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
+
+st.title('Day17: st.secrets')
+
+st.write('This is my secret gitHub username:', st.secrets['gitHub_username'])
+
+st.write(
+	"Has environment variables been set:",
+	os.environ["gitHub_username"] == st.secrets["gitHub_username"], 
+     )
+
+st.title('Day18: st.file_uploader')
+
+st.subheader('Input CSV')
+
+uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     if uploaded_file is not None:
+          df = pd.read_csv(uploaded_file)
+          st.subheader('DataFrame')
+          st.write(df)
+          st.subheader('Descriptive Statistics')
+          st.write(df.describe())
+     else:
+          st.info('☝️ Upload a CSV file')
+
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
+#     # To read file as bytes:
+#     bytes_data = uploaded_file.getvalue()
+#     st.write(bytes_data)
+
+#     # To convert to a string based IO:
+#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+#     st.write(stringio)
+
+#     # To read file as string:
+#     string_data = stringio.read()
+#     st.write(string_data)
+
+#     # Can be used wherever a "file-like" object is accepted:
+#     dataframe = pd.read_csv(uploaded_file)
+#     st.write(dataframe)
+
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     st.write("filename:", uploaded_file.name)
+#     st.write(bytes_data)
+
+st.title('Day 19: How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
+
+
+st.title('Day 21: st.progress')
+
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    tm.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+tm.sleep(1)
+my_bar.empty()
+st.balloons()
+
+st.button("Rerun")
+
